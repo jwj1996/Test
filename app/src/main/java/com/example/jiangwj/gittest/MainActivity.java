6c496969563f386120fc223ff14ddf38ed4e4a9d
jwj1996 <1162117286@qq.com>
2016-07-04 10:12:43 +0800

    111111

---
+++ app/src/main/java/com/example/jiangwj/gittest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jiangwj.gittest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jiangwj on 2016/7/4.
+ */
+public class MainActivity  extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitvity_main);
+    }
+}
